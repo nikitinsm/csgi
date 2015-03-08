986c316c0f4c2967fdf0eada9b6b85c10bcb83e9
nikitinsm <nikitinsm@gmail.com>
2015-03-08 15:06:30 +0300

    ADD: simpliest Hello World example

---
+++ examples/hello_world/server.py
@@ -0,0 +1,24 @@
+from gevent import monkey
+monkey.patch_all()
+
+from csgi import Socket, Listen, env, http
+
+
+def hello_world(env, read, write):
+    write('Hello World!')
+
+
+url_conf = \
+    ( ('/', hello_world)
+    , )
+
+route = env.Router(*url_conf, by=env['http']['path'])
+transport = http.Transport(route)
+
+socket = Socket('tcp://0.0.0.0:9000')
+
+server = Listen(socket, transport)
+
+
+if __name__ == '__main__':
+    server.start()
