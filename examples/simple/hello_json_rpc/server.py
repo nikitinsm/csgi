e863f318e4153a1397e5cdb68fa3d6c7d4b6efed
nikitinsm <nikitinsm@gmail.com>
2015-03-08 17:18:42 +0300

    ADD: simpliest Hello World json rpc example

--- examples/simple/hello_json_rpc/server.py
+++ examples/simple/hello_json_rpc/server.py
@@ -40,16 +40,13 @@ url_conf = \
     ( ('/', api_server)
     , )
 
-route = Router(*url_conf, by=env['http']['path'])
-transport = http.Transport(route)
+router = Router(*url_conf, by=env['http']['path'])
+transport = http.Transport(router)
 
 
 #
 # 4. Start server
 #
 
-socket = Socket('tcp://0.0.0.0:9000')
-server = Listen(socket, transport)
-
 if __name__ == '__main__':
-    server.start()
+    Listen(Socket('tcp://0.0.0.0:9000'), transport).start()
--- examples/simple/hello_world/server.py
+++ examples/simple/hello_world/server.py
@@ -20,16 +20,13 @@ url_conf = \
     ( ('/', hello_world)
     , )
 
-route = Router(*url_conf, by=env['http']['path'])
-transport = http.Transport(route)
+router = Router(*url_conf, by=env['http']['path'])
+transport = http.Transport(router)
 
 
 #
 # 3. Start server
 #
 
-socket = Socket('tcp://0.0.0.0:9000')
-server = Listen(socket, transport)
-
 if __name__ == '__main__':
-    server.start()
+    Listen(Socket('tcp://0.0.0.0:9000'), transport).start()
